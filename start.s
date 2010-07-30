84697223f29de56bddb0fd14d3840519db6a5279
Michael Steil <mist64@mac.com>
2010-07-30 05:14:58 -0700

    7

--- start.s
+++ start.s
@@ -4,33 +4,25 @@ CLK_OUT  := $10 ; bit 4
 VIC_OUT  := $03 ; bits need to be on to keep VIC happy
 
 ;----------------------------------------------------------------------
-; hack to generate .PRG file with load address as first word
+; Hack to generate .PRG file with load address as first word
 ;----------------------------------------------------------------------
 .segment "LOADADDR"
 .addr *
 
 ;----------------------------------------------------------------------
-; main code, lives around $0180
+; Receive one block and run it.
+; This code lives around $0190.
 ;----------------------------------------------------------------------
-.segment "MAIN"
+.segment "PART2"
 main:
-	lda #$0f
-	sta $b9
-	sta $b8
-	ldx #<memory_execute
-	ldy #>memory_execute
-	lda #memory_execute_end - memory_execute
-	jsr $fdf9       ;filnam
-	jsr $f34a       ;open
-
-	lda #DATA_OUT | VIC_OUT ; CLK=0 DATA=1
+	sei
+	lda #VIC_OUT | DATA_OUT ; CLK=0 DATA=1
 	sta $DD00 ; we're not ready to receive
 
 ; wait until fast loader got loaded from 18/18 and is active
 wait_fast:
 	bit $DD00
 	bvs wait_fast
-; the fast code is running now!
 
 	ldx #0
 get_rest_loop:
@@ -39,14 +31,12 @@ get_rest_loop:
 	
 ; wait for raster
 wait_raster:
-	lda $D012
-	cmp #50
-	bcc wait_raster_end
-; XXX this doesn't work right yet :( - restrict to border
-;	and #$07
-;	cmp #$02
-;	beq wait_raster
-	jmp wait_raster
+		lda     $D012
+        cmp     #50
+        bcc     wait_raster_end
+        and     #$07
+        cmp     #$02
+        beq     wait_raster
 wait_raster_end:
 	
 	lda #VIC_OUT ; CLK=0 DATA=0
@@ -64,7 +54,7 @@ wait_raster_end:
 	lsr
 	lsr ; move everything down (bits 0-5)
 	eor $DD00 ; get last 2 bits, now 0-7 are populated
-	ldy #DATA_OUT | VIC_OUT ; CLK=0 DATA=1
+	ldy #VIC_OUT | DATA_OUT ; CLK=0 DATA=1
 	sty $DD00 ; not ready any more, don't start sending
 
 	sta $0400,x
@@ -72,6 +62,22 @@ wait_raster_end:
 	bne get_rest_loop
 
 	jmp *
+	
+;----------------------------------------------------------------------
+; Send an "M-E" to the 1541 that loads track 18, sector 18 into a
+; buffer and executes it.
+; Then jump to code that receives data.
+;----------------------------------------------------------------------
+.segment "START"
+	lda #$0f
+	sta $b9
+	sta $b8
+	ldx #<memory_execute
+	ldy #>memory_execute
+	lda #memory_execute_end - memory_execute
+	jsr $fdf9 ; filnam
+	jsr $f34a ; open
+	jmp main
 
 memory_execute:
 	 .byte "M-E"
@@ -87,9 +93,7 @@ memory_execute_code:
 	jmp $0700
 memory_execute_end:
 
-	
-.segment "START"
-	jmp main
-
+;----------------------------------------------------------------------
+;----------------------------------------------------------------------
 ; C64 -> Floppy: direct
 ; Floppy -> C64: inverted
