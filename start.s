b9db5a4e183118be02b5ef5b0aacb4758b1524e9
Michael Steil <mist64@mac.com>
2010-07-30 04:14:16 -0700

    4

--- start.s
+++ start.s
@@ -1,19 +1,30 @@
 
+; hack to generate .PRG file with load address as first word
 .segment "LOADADDR"
 .addr *
 
 .segment "VECTOR"
-
+; Fill the upper 4 vectors on the stack with $0202
+; this should be enough to catch all different C64
+; configurations (i.e. speeders). If not, we can
+; prepend some more 2s.
 .byte 2,2,2,2,2,2,2,2
 
 .segment "START"
-
-start:
 	nop ; waste 3 bytes - what else could we do? :-)
 	nop
 	nop
 ; this is $0203
 
+	jmp main
+
+.segment "MAIN"
+
+DATA_OUT := $20 ; bit 5
+CLK_OUT  := $10 ; bit 4
+VIC_OUT  := $03 ; bits need to be on to keep VIC happy
+
+main:
 	lda #$0f
 	sta $b9
 	sta $b8
@@ -23,11 +34,6 @@ start:
 	jsr $fdf9       ;filnam
 	jsr $f34a       ;open
 
-; declare VIC bank select pins (#0 & #1)  as input so we don't
-; switch VIC banks all the time when we write to the IEC bus
-	lda #%00111100
-	sta $DD02 ; DDR port A
-	
 	lda #DATA_OUT | VIC_OUT ; CLK=0 DATA=1
 	sta $DD00 ; we're not ready to receive
 
@@ -37,35 +43,6 @@ wait_fast:
 	bvs wait_fast
 ; the fast code is running now!
 
-	jmp get_rest
-	
-memory_execute:
-	 .byte "M-E"
-	 .word $0200 + memory_execute_code - memory_execute
-
-	.byte 18 ; track
-
-memory_execute_code:
-	lda #18 ; track 18, sector 18
-	sta $0e
-	sta $0f
-	lda #4 ; buffer number
-	sta $f9
-	jsr $d586       ; read sector
-	jmp $0700
-
-memory_execute_end:
-
-.segment "MAIN"
-
-;----------------------------------------
-; fast in byte
-;----------------------------------------
-DATA_OUT := $20 ; bit 5
-CLK_OUT  := $10 ; bit 4
-VIC_OUT  := $03 ; bits need to be on to keep VIC happy
-
-get_rest:
 	ldx #0
 get_rest_loop:
 	bit $DD00
@@ -84,10 +61,10 @@ wait_raster:
 wait_raster_end:
 	
 	lda #VIC_OUT ; CLK=0 DATA=0
-	sta $DD00
+	sta $DD00 ; we're ready, start sending!
 	pha ; 3 cycles
 	pla ; 4 cycles
-	bit $EA ; 3 cycles
+	bit $00 ; 3 cycles
 	lda $DD00 ; get 2 bits into bits 6&7
 	lsr
 	lsr ; move down by 2 (bits 4&5)
@@ -98,10 +75,8 @@ wait_raster_end:
 	lsr
 	lsr ; move everything down (bits 0-5)
 	eor $DD00 ; get last 2 bits, now 0-7 are populated
-	pha
-	lda #DATA_OUT | VIC_OUT ; CLK=0 DATA=1
-	sta $DD00
-	pla
+	ldy #DATA_OUT | VIC_OUT ; CLK=0 DATA=1
+	sty $DD00 ; not ready any more, don't start sending
 
 	sta $0400,x
 	inx
@@ -109,9 +84,26 @@ wait_raster_end:
 
 	jmp *
 
+memory_execute:
+	 .byte "M-E"
+	 .word $0200 + memory_execute_code - memory_execute
+	.byte 18 ; track
+memory_execute_code:
+	lda #18 ; track 18, sector 18
+	sta $0e
+	sta $0f
+	lda #4 ; buffer number
+	sta $f9
+	jsr $d586       ; read sector
+	jmp $0700
+memory_execute_end:
+
 ; C64 -> Floppy: direct
 ; Floppy -> C64: inverted
 
 
-; 62 bytes transfer code
-; 66 bytes setup code - max 89 (23 left)
+; 60 bytes transfer code
+; 3+31 bytes setup code +
+; 22 bytes M-E          = 56
+
+; - max 89 (33 left)
